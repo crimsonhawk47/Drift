df19057550850435c6533db23f3ebe0e17f12cda
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-13 00:23:55 -0600

    Added crazy sql query to serve up all chats and their messages/users as needed

--- server/server.js
+++ server/server.js
@@ -70,13 +70,30 @@ io.on("connection", function (socket) {
 function getChats(socket){
   let userId = socket.request.session.passport.user;
 
-  let queryText = `SELECT "chat".id, "messages".message, "user".username, "messages".user_id FROM "chat"
-                    JOIN "messages" ON "messages".chat_id = "chat".id
-                    JOIN "user" ON "messages".user_id = "user".id
-                    WHERE "chat".user1 = $1 OR "chat".user2=$1`
+  //Selects messages and their users by chat and groups them into an array in one column
+  let combineMessagesText = `SELECT "chat".id as "chat_id", ARRAY["messages".message, "user".username] as message_details FROM "chat"
+  JOIN "messages" ON "chat".id = "messages".chat_id
+  JOIN "user" ON "user".id = "messages".user_id
+  WHERE "chat".user1 = $1 OR "chat".user2=$1`
 
-  pool.query(queryText, [Number(userId)])
+  //Creates an array of chat objects with four properties: 
+  //The chat id, an array of messages, and the user ID's of both participants
+  let combineMessagesByChat = `SELECT "chat_id", array_agg("message_details") as chat_messages, "chat".user1, "chat".user2 from (${combineMessagesText})
+                    as foo
+                    JOIN "chat" ON "chat_id" = "chat".id
+                    GROUP BY "chat_id", "chat".user1, "chat".user2
+                    `
+  //Replaces the User ID's of both participants (in the last query) with their actual usernames
+  let fillUsernames = `SELECT "chat_id", "chat_messages", array_agg("user".username) as participants FROM (${combineMessagesByChat})
+                        as foo
+                        JOIN "user" ON "user".id = "user1" OR "user".id = "user2"
+                        GROUP BY "chat_id", "chat_messages";
+                        `
+
+  pool.query(fillUsernames, [Number(userId)])
     .then(response => {
+      console.log(response.rows);
+      
       io.to(socket.id).emit('RECEIVE_ALL_CHATS', response.rows)
     }).catch(error => {
       console.log(error);
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import io from 'socket.io-client'
+import { json } from 'body-parser';
 
 
 class Chat extends Component{
@@ -8,9 +9,10 @@ class Chat extends Component{
     render(){
         return(
         <div>
-            {this.props.reduxStore.chats.map((text, index)=>{
+            {/* {this.props.reduxStore.chats.map((chat, index)=>{
                 return <p key={index}>{text.username}: {text.message}</p>
-            })}
+            })} */}
+            {JSON.stringify(this.props.reduxStore.chats)}
         </div>
         )
     
