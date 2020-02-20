5a64502e96feeea3127328085de9d55b50f1c13b
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-20 13:51:42 -0600

    created Loading Component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import { Paper, Grid, Typography } from '@material-ui/core'
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  }
+});
+
+class Loading extends Component {
+  //Will run if reduxUpdates
+  componentDidUpdate(){
+    this.props.history.push('/home')
+  }
+
+  
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Grid container className={classes.root}>
+        <Typography >
+          I am a Loading Component
+                </Typography>
+      </Grid>
+    )
+
+  }
+}
+
+const mapStateToProps = reduxStore => {
+  return (
+    { reduxStore }
+  )
+}
+
+export default withStyles(styles)(connect(mapStateToProps)(Loading))
