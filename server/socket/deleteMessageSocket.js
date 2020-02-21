189dc7c4d1d843e5c9f2fde5e33aabf03067a8a5
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-21 00:30:33 -0600

    scrollbar added

--- server/socket/deleteMessageSocket.js
+++ server/socket/deleteMessageSocket.js
@@ -2,14 +2,26 @@ const pool = require('../modules/pool')
 
 const deleteMessage = (socket, io, serverMethods) => {
 
-  socket.on('DELETE_MESSAGE', data => {
-    console.log(`IN DELETE_MESSAGE`);
+  socket.on('DELETE_MESSAGE', async data => {
+    console.log(`IN DELETE_MESSAGE ${data}`);
+
+    try {
+      let isChatActive = await pool.query(`SELECT * FROM "chat"
+                          WHERE "chat".id = $1`, [data.chatId])
+      if (!isChatActive.rows[0].active) {
+        return 0
+      }
+    } catch (err) {
+      console.log(err);
+      return err
+
+    }
 
     let userId = socket.request.session.passport.user
     let room;
     //Grab the chat_id, which is synonymous with the room number
     pool.query(`SELECT "chat_id" FROM "messages"
-                    WHERE "messages".id = $1 AND "messages".user_id = $2`, [data, userId])
+                    WHERE "messages".id = $1 AND "messages".user_id = $2`, [data.id, userId])
       .then(result => {
         //There should only be one result, with a column of chat_id. This is the room
         room = String(result.rows[0].chat_id)
@@ -20,7 +32,7 @@ const deleteMessage = (socket, io, serverMethods) => {
           console.log(`About to DELETE`);
           let queryText = `DELETE FROM "messages"
                                 WHERE "messages".id = $1 AND "messages".user_id = $2`
-          pool.query(queryText, [data, userId])
+          pool.query(queryText, [data.id, userId])
             .then(result => {
               console.log(result);
               //After deleting the message, we tell everyone in the room to update
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import { withStyles } from '@material-ui/core/styles'
 import { connect } from 'react-redux'
 import moment from 'moment'
+import { Element } from 'react-scroll';
 
 const styles = theme => ({
 
@@ -15,14 +16,23 @@ const styles = theme => ({
   },
   rightChat: {
 
+  },
+  scroll: {
+    overflow: 'scroll',
+    height: '400px',
+    margin: '20px'
   }
 });
 
 
+
+
 class Chat extends Component {
 
+
+
   state = {
-    input: ''
+    input: '',
   }
 
   goHome = () => {
@@ -35,30 +45,46 @@ class Chat extends Component {
     })
   }
 
-  deleteMessage = (id) => {
+  deleteMessage = (id, chatId) => {
     console.log(`DELETING`);
     this.props.dispatch({
       type: 'DELETE_MESSAGE',
-      payload: id
+      payload: { id, chatId }
     })
 
+  }
+
+  scrollToBottom = () => {
+    let scrollAnchor = document.getElementById("scroll-anchor");
+    if (scrollAnchor) {
+      scrollAnchor.scrollTo({
+        top: scrollAnchor.scrollHeight,
+        behavior: 'auto'
+      })
+
+    }
   }
 
   sendMessage = (chatId) => {
     this.props.dispatch({ type: 'SEND_MESSAGE', payload: { input: this.state.input, chatId: chatId } })
     this.setState({ input: '' })
+
   }
 
   render() {
 
 
 
+    if (this.messagesEnd) {
+
+    }
     const { classes } = this.props;
 
     let index = this.props.match.params.index
     let chat = this.props.reduxStore.chats[index]
 
     let chat_id = chat && chat.chat_id
+    let active = chat && chat.active
 
     let myUser = this.props.reduxStore.user.username
     let partner;
@@ -71,39 +97,45 @@ class Chat extends Component {
 
     }
 
-
-
     return (
       <Grid container className={classes.root} spacing={2} justify='center'>
-        {chat && chat.chat_messages.map((messageData, index) => {
-          let message = messageData.message
-          let userSpeaking = messageData.username
-          let date = messageData.date
-          
-          date = moment(date).format('LT, LL')
-          
-          let img = messageData.img
-          return (
-            <Grid item xs={7} key={index}>
-              <Grid container spacing={0} justify='flex-start'>
-                <Avatar src={img}></Avatar>
-                <Paper>
-                  {userSpeaking === myUser ?
-                    <DeleteIcon onClick={() => this.deleteMessage(messageData.id)} /> : <p></p>}
-                  <Typography>{userSpeaking}: {message} ({date})</Typography>
-                </Paper>
+        <div className={classes.scroll} id='scroll-anchor'>
+          {chat && chat.chat_messages.map((messageData, index) => {
+            let message = messageData.message
+            let userSpeaking = messageData.username
+            let date = messageData.date
+
+            date = moment(date).format('LT, LL')
+
+            let img = messageData.img
+            return (
+              <Grid item xs={7} key={index}>
+                <Grid container spacing={0} justify='flex-start'>
+                  <Avatar src={img}></Avatar>
+                  <Paper>
+                    {userSpeaking === myUser && active ?
+                      <DeleteIcon onClick={() => this.deleteMessage(messageData.id, chat_id)} /> : <p></p>}
+                    <Typography>{userSpeaking}: {message} ({date})</Typography>
+                  </Paper>
+                </Grid>
               </Grid>
-            </Grid>
-          )
-        })}
+            )
+          })}
+        </div>
+        <div ref={this.messagesEndRef}></div>
         <Grid item xs={11} container justify="center">
-          <Input
-            onChange={(event) => { this.handleChangeFor(event, 'input') }}
-            placeholder='Send a message'
-            value={this.state.input}
-            fullWidth />
-          <Button onClick={() => { this.sendMessage(chat_id) }}>Send Message</Button>
-          <Button onClick={this.goHome}>GO BACK HOME</Button>
+          {active ?
+            <div>
+              <Input
+                onChange={(event) => { this.handleChangeFor(event, 'input') }}
+                placeholder='Send a message'
+                value={this.state.input}
+                fullWidth />
+              <Button onClick={() => { this.sendMessage(chat_id) }}>Send Message</Button>
+              <Button onClick={this.goHome}>GO BACK HOME</Button>
+            </div> :
+            <p></p>}
+
         </Grid>
 
 
