894caf3db58cdcf25b7811be9c4bf91ecd216f95
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-05-17 14:51:59 -0500

    Added redux devtools and constant for redux Logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,25 +3,30 @@ import rootSaga from './sagas/index'; // imports ./redux/sagas/index.js
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewareList = process.env.NODE_ENV === 'development' ?
-  [sagaMiddleware, logger] :
+const enableLogger = false;
+
+let middlewareList = process.env.NODE_ENV === 'development' ?
+  [sagaMiddleware,] :
   [sagaMiddleware];
 
+if (enableLogger) middlewareList = [...middlewareList, logger]
 
-  const store = createStore(
-    // tells the saga middleware to use the rootReducer
-    // rootSaga contains all of our other reducers
-    rootReducer,
-    // adds all middleware to our project including saga and logger
-    // applyMiddleware(...middlewareList),
-    applyMiddleware(...middlewareList),
+const store = createStore(
+  // tells the saga middleware to use the rootReducer
+  // rootSaga contains all of our other reducers
+  rootReducer,
+  // adds all middleware to our project including saga and logger
+  // applyMiddleware(...middlewareList),
+  composeWithDevTools(applyMiddleware(...middlewareList)),
 
-  );
-    
+);
+
 sagaMiddleware.run(rootSaga)
 
 export default store
