4a49bb6cc7eb954a6e634f218eeca7053fea8d55
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-05-17 12:27:24 -0500

    Refactored delete message to use AWAIT

--- server/socket/deleteMessage.js
+++ server/socket/deleteMessage.js
@@ -10,39 +10,26 @@ const deleteMessage = (socket, io, serverMethods) => {
       await serverMethods.isChatActive(chatId)
 
       //Grab the chat_id, which is synonymous with the room number
-      pool.query(`SELECT "chat_id" FROM "messages"
+      const result = await pool.query(`SELECT "chat_id" FROM "messages"
     WHERE "messages".id = $1 AND "messages".user_id = $2`, [data.id, userId])
-        .then(result => {
-          //There should only be one result, with a column of chat_id. This is the room
-          room = String(result.rows[0].chat_id)
-          console.log('Setting Room to ', room);
+      //There should only be one result, with a column of chat_id. This is the room
+      room = String(result.rows[0].chat_id)
+      console.log('Setting Room to ', room);
 
-          //If we actually got the room the message is from
-          if (room) {
-            console.log(`About to DELETE`);
-            let queryText = `DELETE FROM "messages"
+      //If we actually got the room the message is from
+      if (room) {
+        let queryText = `DELETE FROM "messages"
         WHERE "messages".id = $1 AND "messages".user_id = $2`
-            pool.query(queryText, [data.id, userId])
-              .then(result => {
-                console.log(result);
-                //After deleting the message, we tell everyone in the room to update
-                io.to(room).emit('GET_MESSAGES')
+        const deleteResult = await pool.query(queryText, [data.id, userId])
+        //After deleting the message, we tell everyone in the room to update
+        io.to(room).emit('GET_MESSAGES')
 
-              }).catch(err => {
-                console.log(err);
-              })
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        })
+      }
     }
     catch (err) {
       console.log(err);
       return err
-
     }
-
   })
 }
 module.exports = deleteMessage
