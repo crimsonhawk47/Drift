db8696d9e97527c26de05b0f490f34950d011902
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-05-17 15:01:15 -0500

    Moved the users name display into its own component

---
+++ src/components/UserHeaderDisplay/UserHeaderDisplay.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux'
+import { Grid, Typography, Box } from '@material-ui/core'
+import { Avatar } from '@material-ui/core'
+
+
+
+const UserHeaderDisplay = (props) => {
+    return (
+        <Grid container spacing={6} alignItems='center'>
+            <Grid item xs={1}>
+                <Avatar component='span' onClick={() => { props.history.push('/avatar') }} src={props.user.image} />
+            </Grid>
+            <Grid item xs={2}>
+                <Box fontStyle="italic" fontWeight={100} fontSize={21} fontFamily="Roboto">
+                    <Typography display='inline' variant='p' id="welcome">
+                        {props.user.username}
+                    </Typography>
+                </Box>
+            </Grid>
+        </Grid>
+
+    )
+
+}
+
+const mapStateToProps = reduxStore => {
+    return (
+        { user: reduxStore.user }
+    )
+}
+export default connect(mapStateToProps)(UserHeaderDisplay)
