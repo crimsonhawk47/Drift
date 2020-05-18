aa7b3c46bcad510a29ab8f9059f07648b6b6c475
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-05-18 02:44:34 -0500

    Added partner block to chat list SQL return

--- server/modules/serverMethods/getChats.js
+++ server/modules/serverMethods/getChats.js
@@ -7,26 +7,29 @@ function getChats(socket) {
     // console.log(`Moment before getChats Query says ${moment().format('MMMM Do YYYY, h:mm:ss.SSS a')}`);
 
     //Selects messages and their users by chat and groups them into an array in one column
-    let combineMessagesText = `SELECT "chat".id as "chat_id", jsonb_build_object('id', "messages".id, 'message', "messages".message, 'username', "user".username, 'date', "messages".date, 'img', "user".image) as message_details, "messages".id as "message_id" FROM "chat"
+    let combineMessagesText = `SELECT "chat".id as "chat_id", jsonb_build_object('id', "messages".id, 'message', "messages".message, 'username', "user".username, 'date', "messages".date, 'img', "user".image) as message_details, "messages".id as "message_id", (CASE when "messages".user_id=1 THEN true ELSE false END) as "owner" FROM "chat"
     JOIN "messages" ON "chat".id = "messages".chat_id
     JOIN "user" ON "user".id = "messages".user_id
     WHERE ("chat".user1 = $1 OR "chat".user2=$1)
-    ORDER BY "messages".id`
+    ORDER BY "messages".id
+    `
 
     //Creates an array of chat objects with four properties: 
     //The chat id, an array of messages, and the user ID's of both participants
-    let combineMessagesByChat = `SELECT "chat_id", "chat".start_date as "chat_date", jsonb_agg("foo".message_details ORDER BY foo."message_id") as chat_messages, "chat".user1, "chat".user2, "chat".active from (${combineMessagesText})
-                                as foo
-                                JOIN "chat" ON "chat_id" = "chat".id
-                                GROUP BY "chat_id", "chat".user1, "chat".user2, "chat".active, "chat".start_date
-                                `
+    let combineMessagesByChat = `SELECT "chat_id", "chat".start_date as "chat_date", jsonb_agg("foo".message_details ORDER BY foo."message_id") as chat_messages, "chat".user1, "chat".user2, "chat".active 
+    from (${combineMessagesText}) as foo
+    JOIN "chat" ON "chat_id" = "chat".id
+    GROUP BY "chat_id", "chat".user1, "chat".user2, "chat".active, "chat".start_date`
+                                
     //Replaces the User ID's of both participants (in the last query) with their actual usernames
     //Also places both usernames in a single array
-    let fillUsernames = `SELECT "chat_id", "chat_date", "chat_messages", array_agg("user".username) as participants, "goo".active FROM (${combineMessagesByChat})
-                        as goo
-                        JOIN "user" ON "user".id = "user1" OR "user".id = "user2"
-                        GROUP BY "chat_id", "chat_messages", "goo".active, "goo".chat_date
-                        ORDER BY "goo".active DESC, "goo".chat_date
+    let fillUsernames = `SELECT "chat_id", "chat_date", "chat_messages", array_agg("user".username) as participants, "goo".active,
+    jsonb_build_object('image', "user".image, 'name', "user".username) as "partner"
+    FROM (${combineMessagesByChat}) as goo
+    JOIN "user" ON "user".id = "user1" OR "user".id = "user2"
+    WHERE "user".id != $1
+    GROUP BY "chat_id", "chat_messages", "goo".active, "goo".chat_date, "user".id
+    ORDER BY "goo".active DESC, "goo".chat_date
                         `
 
     pool.query(fillUsernames, [Number(userId)])
